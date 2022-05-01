7922fccd19e637d33941b8c656a27a40c9997608
Pheng <phengyangster9@gmail.com>
2022-05-01 13:57:15 -0700

    code

--- lab_5.py
+++ lab_5.py
@@ -36,3 +36,22 @@ create_dictionary(infile):
  
 main()
 """
+
+def main():
+    sentence = input("Enter a sentence")
+    dictionary = create_dictionary("textese.txt")
+    translate(sentence, dictionary)
+ 
+def create_dictionary(txt_file):
+    infile = open(txt_file, "r")
+    words = [word.rstrip() for word in infile]
+    infile.close()
+    return dict([word.split(",") for word in words])
+ 
+def translate(sentence, dictionary):
+    # print("From translate", sentence)
+    words = sentence.split()
+    for word in words:
+        print(dictionary.get(word, word), " ", end="")
+       
+main()
