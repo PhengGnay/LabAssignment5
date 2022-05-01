e195ec60e9f23b49885dc7a11063964d1679c273
Pheng <phengyangster9@gmail.com>
2022-05-01 13:53:38 -0700

    structured-english

--- lab_5.py
+++ lab_5.py
@@ -1,5 +1,3 @@
-
-
 """
 The program is trying to translate a sentence captured as user input.
 We first read in the text file textese.txt.
@@ -7,7 +5,7 @@ Then from the text file, we create a list of strings from each line on the text 
 Then, we create a dictionary the list.
 Once the text file has been read into memory, we close the file.
 
-We then decline a functions for translating which envolves splitting the user input (sentence) into an
+We then decline a functions for translating which involves splitting the user input (sentence) into an
 array of strings ("enjoy the excellent band tonight") ["enjoy", "the", "excellent", "band", "tonight"]
 
 Once we have the array of strings representing the user's input sentence, we 
@@ -17,44 +15,3 @@ in our dictionary.
 After each word is rtranslated, we then
 print out the translated sentence to the user.
 """
-
-"""
-main():
-    set sentence = input()
-    set dictionary = create_dictionay()
-    translate(sentence, dictionary)
-
-translate(sentence, dictionary):
-    words = for each of the word in the sentence
-    for each words, translate the word
-    print translated sentence to user
-
-create_dictionary(infile):
-    read in textese.txt
-    create list = each line from file
-    close the file
-    create a dict off of the list
-    retuen the dict
-
-main()
-"""
-
-
-def main():
-    sentence = input("Enter a sentence")
-    dictionary = create_dictionary("textese.txt")
-    translate(sentence, dictionary)
-
-def create_dictionary(txt_file):
-    infile = open(txt_file, "r")
-    words = [word.rstrip() for word in infile]
-    infile.close()
-    return dict([word.split(",") for word in words])
-
-def translate(sentence, dictionary):
-    # print("From translate", sentence)
-    words = sentence.split() 
-    for word in words:
-        print(dictionary.get(word, word), " ", end="") 
-        
-main()
