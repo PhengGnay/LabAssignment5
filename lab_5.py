8149808dc3de62b28ff3363a9d55d7ad0613f869
Pheng <phengyangster9@gmail.com>
2022-05-01 13:55:50 -0700

    pseudocode

--- lab_5.py
+++ lab_5.py
@@ -15,3 +15,24 @@ in our dictionary.
 After each word is rtranslated, we then
 print out the translated sentence to the user.
 """
+
+"""
+main():
+    set sentence = input()
+    set dictionary = create_dictionay()
+    translate(sentence, dictionary)
+ 
+translate(sentence, dictionary):
+    words = for each of the word in the sentence
+    for each words, translate the word
+    print translated sentence to user
+ 
+create_dictionary(infile):
+    read in textese.txt
+    create list = each line from file
+    close the file
+    create a dict off of the list
+    retuen the dict
+ 
+main()
+"""
